763a662020b18596a896715f68281da26db9b512
Robert <16901597+aquaflamingo@users.noreply.github.com>
2022-03-15 08:51:14 -0700

    Adds node and syntax tree

---
+++ src/node.cr
@@ -0,0 +1,7 @@
+class Node
+	 def initialize(@left: Node?, @right: Node, @item: Element); end
+
+	 property :left
+	 property :right
+	 property :item
+end
---
+++ src/syntax_tree.cr
@@ -0,0 +1,8 @@
+class SyntaxTree
+	 class Builder
+			class << self
+				 def build(elements: Array<String>)
+				 end
+			end
+	 end
+end
