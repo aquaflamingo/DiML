a15356a016b18778107bfe5ec5b4789916c47812
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-07-07 14:54:58 -0400

    fixup! Basic tree building

--- lib/diml/document.rb
+++ lib/diml/document.rb
@@ -6,15 +6,14 @@ require_relative "./parser"
 require_relative "./content_tree"
 
 class Document
-  def initialize(content)
-    @content = content
+  def initialize(content_tree)
+    @tree = content_tree
   end
 
   def self.load(path)
     c = File.read(path)
 
     p = Parser.new(c)
-    binding.pry
     p.parse
   end
 end
